367261855fb9206be7435d090df25627f00b2ea3
alexpryp <alexpryp@gmail.com>
2019-07-26 20:36:25 +0300

    Created PC configurator

---
+++ dist/js/script.js
@@ -0,0 +1,30 @@
+"use strict";
+"use sctrict";
+
+$('document').ready(function () {
+  var $chassisImgHolder = $('#imgHolder img'); // 1. Получение значения атрибута src у картинки
+
+  var srcValue = $chassisImgHolder.attr('src'); // 2. Поменяем значение атрибута src у картинки
+
+  $chassisImgHolder.on('click', function () {
+    $(this).fadeOut(300, function () {
+      $(this).attr('src', 'chassis/Thermaltake Versa H13 Black_Win (CA-1D3-00S1WN-00).jpg').fadeIn(300);
+    });
+  });
+  /*// 3. ОТДЕЛЬНОЕ действие для каждого корпуса
+  $('#chassisSelector .chassisItem.chassis1').on('click', function(){
+  	$('#imgHolder img').attr('src', 'chassis/Thermaltake Versa H13 Black_Win (CA-1D3-00S1WN-00).jpg');
+  });
+  $('#chassisSelector .chassisItem.chassis2').on('click', function(){
+  	$('#imgHolder img').attr('src', 'chassis/Thermaltake Versa C22 RGB (CA-1G9-00M1WN-00).jpg');
+  });*/
+  // 4. Единое действие для всех элементов с анимацией
+
+  $('#chassisSelector .chassisItem').on('click', function () {
+    var imgPath;
+    imgPath = $(this).attr('data-img-path');
+    $chassisImgHolder.fadeOut(300, function () {
+      $(this).attr('src', imgPath).fadeIn(300);
+    });
+  });
+});
---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const uglify = require('gulp-uglify');
+const del = require('del'); //удаляет указанные файлы и директории
+const babel = require('gulp-babel');
+
+sass.compiler = require('node-sass');
+
+function styles() {
+	return gulp.src('./src/sass/main.scss') //берём файлы на обработку
+				.pipe(sass().on('error', sass.logError)) //компиляция из scss в css
+				.pipe(autoprefixer({  //расстановка префиксов
+		            overrideBrowserlist: ['> 1%'],
+		            cascade: false
+		        }))
+				.pipe(gulp.dest('./dist/css')); //сохраняем обработанные файлы
+}
+
+function scripts() {
+	return gulp.src('./src/js/script.js') //берём файлы на обработку
+				.pipe(babel({
+           			presets: ['@babel/preset-env']
+        		}))				
+				.pipe(gulp.dest('./dist/js')); //сохраняем обработанные файлы
+}
+
+function watch() {   // создание таска, отслеживающего изменения файлов и, при наличии изменений, запускающего указанные обработчики-таски
+	gulp.watch('./src/sass/**/*.scss', styles); //в случае изменений в исходниках scss, автоматически запускает на выполнение таск styles
+	gulp.watch('./src/js/**/*.js', scripts);  //в случае изменений в исходниках js, автоматически запускает на выполнение таск scripts
+}
+
+function clean() {
+	return del(['build/css/*', 'build/js/*']); //удаление файлов в указанных директориях
+}
+
+
+gulp.task('styles', styles); //регистрация таска styles
+gulp.task('scripts', scripts); //регистрация таска scripts
+gulp.task('watch', watch); //регистрация таска watch
+gulp.task('clean', clean); //регистрация таска clean
