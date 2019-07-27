3811bea4cd98a829d901ae957bd99ea4bd9cd3f5
alexpryp <alexpryp@gmail.com>
2019-07-27 09:33:01 +0300

    Added section accessories

--- src/js/script.js
+++ src/js/script.js
@@ -1,29 +1,16 @@
-"use sctrict"
+"use strict"
 
 $('document').ready(function(){
 	var $chassisImgHolder = $('#imgHolder img');
 
-	// 1. Получение значения атрибута src у картинки
-
 	var srcValue = $chassisImgHolder.attr('src');
 
-	// 2. Поменяем значение атрибута src у картинки
 	$chassisImgHolder.on('click', function() {
 		$(this).fadeOut(300, function(){
 			$(this).attr('src', 'chassis/Thermaltake Versa H13 Black_Win (CA-1D3-00S1WN-00).jpg').fadeIn(300);
 		});
 	});
 
-	/*// 3. ОТДЕЛЬНОЕ действие для каждого корпуса
-	$('#chassisSelector .chassisItem.chassis1').on('click', function(){
-		$('#imgHolder img').attr('src', 'chassis/Thermaltake Versa H13 Black_Win (CA-1D3-00S1WN-00).jpg');
-	});
-	$('#chassisSelector .chassisItem.chassis2').on('click', function(){
-		$('#imgHolder img').attr('src', 'chassis/Thermaltake Versa C22 RGB (CA-1G9-00M1WN-00).jpg');
-	});*/
-
-	
-	// 4. Единое действие для всех элементов с анимацией
 	$('#chassisSelector .chassisItem').on('click', function(){
 		var imgPath;
 
@@ -33,5 +20,50 @@ $('document').ready(function(){
 			$(this).attr('src', imgPath).fadeIn(300);
 		});
 	});
+
+	var modelSpecs = "",
+		modelPrice = 0,
+		modelSpecsHolder,
+		modelPriceHolder
+
+	modelSpecsHolder = $('#modelSpecs');
+	modelPriceHolder = $('#modelPrice');
+
+	function calculatePrice() {
+		modelPrice = +$('input[name=cpu]:checked', '#autoForm').val();
+		modelPrice += +$('input[name=gpu]:checked', '#autoForm').val();
+		modelPrice += +$('input[name=ssd]:checked', '#autoForm').val();
+
+		modelPriceHolder.text(addSpace(modelPrice) + ' грн');
+	}
+
+	function compileSpecs() {
+		modelSpecs = $('input[name=cpu]:checked + label', '#autoForm').text() + ' / ';
+		modelSpecs += $('input[name=gpu]:checked + label', '#autoForm').text() + ' / ';
+		modelSpecs += $('input[name=ssd]:checked + label', '#autoForm').text();
+
+		modelSpecsHolder.text(modelSpecs);
+	};
+
+	$('#autoForm input').on('change', function(){
+		calculatePrice();
+		compileSpecs();
+	});
+
+	calculatePrice();
+	compileSpecs();
+
+	function addSpace(nStr) {
+		nStr += '';
+		var x = nStr.split('.');
+		var x1 = x[0];
+		var x2 = x.length > 1 ? '.' + x[1] : '';
+		var rgx = /(\d+)(\d{3})/;
+		while(rgx.test(x1)) {
+			x1 = x1.replace(rgx, '$1' + ' ' + '$2');
+		}
+
+		return x1 + x2;
+	}
 
 });
